8127c08f147522bc2a461dcbca8735fe1917f5de
Eddie Penta <eddie@gamezgalaxy.com>
2013-02-08 22:20:59 -0500

    Updated smack dependency

--- src/com/ep/hippyjava/test/Main.java
+++ src/com/ep/hippyjava/test/Main.java
@@ -5,7 +5,7 @@ import com.ep.hippyjava.HippyJava;
 public class Main {
     
     public static void main(String[] args) {
-        HippyJava.runBotDesync(new MyTestBot());
+        HippyJava.runBot(new MyTestBot());
     }
 
 }
